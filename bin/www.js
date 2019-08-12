001283d306b7346e9b401b59bb01d649cc5214b7
diego <furtado.diegos@gmail.com>
2019-08-12 00:08:58 -0300

    new heroku settings

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,13 @@
 /**
  * Dependencies
  */
+const http = require('http');
 const { app } = require('../app');
 
 const PORT = process.env.PORT || 5000;
 
+// Start HTTP server
+const server = http.createServer(app);
+
 // Listening the app
-app.listen(PORT, () => console.log(`Api running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Api running on port: ${PORT}`));
